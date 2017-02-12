7121503e62883305b1d44a6ae1ac5ae7c366cc7b
johndudex <johndudex@gmail.com>
2017-02-12 18:25:44 +0530

    sql file

--- populate_college_table.py
+++ populate_college_table.py
@@ -20,12 +20,15 @@ def main():
 	soup = BeautifulSoup(page,'lxml')
 	tables= soup.findAll("table",{"class":"table table-striped table-hover"})
 
-	for table in tables:
-		#pdb.set_trace()
+	 table in tables:
+	#pdb.set_trace()
 		row  = table.findAll("tr")
 		for i in  row:
 			td_tag = i.findAll('td')
 			if td_tag:
-				sql = "insert into college (college_name, college_prefix) values(%s,%s)" %(td_tag[1],td_tag[2])
+				sql = "insert into college (college_name, college_prefix) values(%s,%s)" %(td_tag[1].text,td_tag[2].text)
 				cursor.execute(sql)
-			
+
+
+if __name__ == '__main__':
+	main()
