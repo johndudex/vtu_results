0e7524accb11577f00d170f50f6e5c71cc97c42d
johndudex <johndudex@gmail.com>
2017-02-12 18:16:25 +0530

    sql file

---
+++ populate_college_table.py
@@ -0,0 +1,31 @@
+#import request
+import urllib2
+from bs4 import BeautifulSoup
+import pdb
+import MySQLdb
+
+connection = MySQLdb.connect(host="localhost",    # your host, usually localhost
+                     user="root",         # your username
+                     passwd="john",  # your password
+                     db="vtu_results")        # name of the data base
+
+cursor = connection.cursor()
+hdr = {'User-Agent': 'Mozilla/6.0'}
+site = "http://www.fastvturesults.com/college_codes"
+
+def main():
+
+	requ= urllib2.Request(site,headers=hdr)
+	page = urllib2.urlopen(requ)
+	soup = BeautifulSoup(page,'lxml')
+	tables= soup.findAll("table",{"class":"table table-striped table-hover"})
+
+	for table in tables:
+		#pdb.set_trace()
+		row  = table.findAll("tr")
+		for i in  row:
+			td_tag = i.findAll('td')
+			if td_tag:
+				sql = "insert into college (college_name, college_prefix) values(%s,%s)" %(td_tag[1],td_tag[2])
+				cursor.execute(sql)
+			
